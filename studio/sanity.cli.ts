fd12c168a69a5a2d517772c1992ba2e5337d990e
BuildMyPc <info@buildmypc.in>
2026-05-14 13:03:02 +0530

    chore: save studioHost + appId in cli config, fix styled-components version

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -5,4 +5,8 @@ const dataset = process.env.SANITY_STUDIO_DATASET || 'production';
 
 export default defineCliConfig({
   api: { projectId, dataset },
+  studioHost: 'abhayoyun',
+  deployment: {
+    appId: 'kv132z9bw6gvzwzp7bh3b1i3',
+  },
 });
